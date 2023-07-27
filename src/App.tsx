55b70fc55db3abf8c682d95601f7b37132921206
EstivenCano <estiven.canou@perficient.com>
2023-07-26 20:11:54 -0500

    [Add] Theme css file and Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "./components/button";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <main className='main_container'>
+      <h1>Figma Integrations</h1>
+
+      <section className='card'>
+        <h2>Storybook</h2>
+
+        <div>
+          <h3>Primary</h3>
+          <Button variation='primary'>Button</Button>
+        </div>
+        <div>
+          <h3>Secondary</h3>
+          <Button variation='secondary'>Button</Button>
+        </div>
+
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </section>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </main>
   );
 }
 
